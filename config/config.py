0ec8aa6e00215637bb23879e22eb22281e5fb847
kart2953 <karthikkannan61@gmail.com>
2026-01-09 17:30:27 +0530

    Modified config file

--- config/config.py
+++ config/config.py
@@ -1,5 +1,4 @@
 class Config:
     BASE_URL = "https://jsonplaceholder.typicode.com"
     BASE_URL_2 = "https://api.openweathermap.org/data/2.5"
-    BASE_URL_3 = "https://dummyjson.com"
-    BASE_URL_4 = "https://api.github.com"
+    BASE_URL_3 = "https://fakestoreapi.com"
